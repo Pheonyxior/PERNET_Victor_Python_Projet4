ac6aeb438f28776e50c1602301b192d277926890
Victor PERNET <victor@av-connexion.fr>
2025-06-13 08:56:38 +0200

    notes

---
+++ Notes.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+
+# TODO : Ajouter fonction afficher résultats tournoi terminé
+# TODO : Voir comment initialiser les json databases s'ils n'existent pas
+# TODO : Voir s'il faut ajouter le setup.cfg au gitignore
+# TODO : Faire le requirements.txt
+# TODO : Parser les inputs de l'utilisateur pour s'assurer qu'elles sont corrects (dates ect...)
+# TODO : Gérer quand un utilisateur s'apprete à overwrite une entrée de tournoi existant dans la database
+# TODO : 
+# TODO : 
+# TODO : 
+# TODO : 
+# TODO : confirmation flake8
+
+# * DONE : 
+# * DONE : Pouvoir interrompre un match et le reprendre
+# * DONE : Mettre le jour en plus de l'heure pour les dates
+# * DONE : Joueurs enregistrés et Tours et nombre de tours affichés quand tournois sélectionné
+# * DONE : Fonction afficher les tours
+# * DONE : 
+# * DONE : fixer ajouter un tournoi
+# * DONE : Simulation de tournois
+# * DONE : Mettre à jour le score des joueurs pour le prochain tour
+# * DONE : Simuler des matchs
+# * DONE : Faire des paires de joueurs
+# * DONE : Trier les joueurs d'un tournoi par score
+# * DONE : Contrôler si joueur est déjà inscrit au tournois
+# * DONE : Fonction tournament list
+# * DONE : Vérifier si clé existe dans tour nlist
+# * DONE : Voir pourquoi start et end date sont ajoutés en tant qu'array de string
+# * DONE : en lever les espaces des clés
+# * DONE : load et write tournament et player database
+# * DONE : rand tournament et player
+# * DONE : 
